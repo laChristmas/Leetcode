6a5079dadc0a658b63fa463a9bab77aa79a48f09
Zeyu Yang <1124159491@qq.com>
2026-05-13 22:23:18 +1000

    Exercise 2235 completed

---
+++ 2235_addTwoIntegers_1.java
@@ -0,0 +1,6 @@
+/**Welcome back to where you began*/
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ 2235_addTwoIntegers_1.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+# Welcome back to where you began
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
